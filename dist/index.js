801d28983fc2ecc76056ed8f7b2e9706192cbd99
unknown <anika01839@gmail.com>
2024-11-27 21:15:43 +0600

    data fetch and add ui

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,37 @@
 "use strict";
-console.log("hello world");
+const getUsername = document.querySelector("#user");
+const formSubmit = document.querySelector("#form");
+const main_container = document.querySelector(".main_container");
+// reusable function
+async function myCustomFetcher(url, options) {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+        throw new Error(`NetWork is not ok-- ${response.status}`);
+    }
+    const data = response.json();
+    console.log(data);
+    return data;
+}
+// display the card ui
+const showResultUI = (singleUser) => {
+    const { avatar_url, login, url } = singleUser;
+    main_container.insertAdjacentHTML("beforeend", `
+    <div class="card">
+        <img src=${avatar_url} alt=${login} />
+        <hr />
+        <div class="card-footer">
+            <img src="${avatar_url}" alt="${login}" /> 
+            <a href="${url}"> Github </a>
+        </div>
+    </div>
+    `);
+};
+function fetchUserData(url) {
+    myCustomFetcher(url, {}).then((userInfo) => {
+        for (const singleUser of userInfo) {
+            showResultUI(singleUser);
+        }
+    });
+}
+// default function call
+fetchUserData("https://api.github.com/users");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,55 @@
-console.log("hello world");
+const getUsername = document.querySelector("#user") as HTMLInputElement;
+const formSubmit = document.querySelector("#form") as HTMLFormElement;
+const main_container = document.querySelector(".main_container") as HTMLElement;
+
+// contract definds
+interface UserData {
+  id: number;
+  login: string;
+  avatar_url: string;
+  location: string;
+  url: string;
+}
+
+// reusable function
+async function myCustomFetcher<T>(
+  url: string,
+  options?: RequestInit
+): Promise<T> {
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    throw new Error(`NetWork is not ok-- ${response.status}`);
+  }
+  const data = response.json();
+  console.log(data);
+  return data;
+}
+
+// display the card ui
+const showResultUI = (singleUser: UserData) => {
+  const { avatar_url, login, url } = singleUser;
+  main_container.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class="card">
+        <img src=${avatar_url} alt=${login} />
+        <hr />
+        <div class="card-footer">
+            <img src="${avatar_url}" alt="${login}" /> 
+            <a href="${url}"> Github </a>
+        </div>
+    </div>
+    `
+  );
+};
+
+function fetchUserData(url: string) {
+  myCustomFetcher<UserData[]>(url, {}).then((userInfo) => {
+    for (const singleUser of userInfo) {
+      showResultUI(singleUser);
+    }
+  });
+}
+// default function call
+
+fetchUserData("https://api.github.com/users");
